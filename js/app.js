f1af4017e4a524c188ca7b83ed67806d5072e897
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-12-13 07:16:21 +0100

    Download flag from API. Try to add map from Leaflet.js library

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const select = document.querySelector("#countrySelect");
-const countryData = document.querySelector("#countryData")
+const countryData = document.querySelector("#countryData");
+const flagImg = document.querySelector("#countryFlag");
+let countryMap = document.querySelector("#mapid");
 
 fetch('https://restcountries.eu/rest/v2/all?fields=iso2Code;name')
     .then(resp =>
@@ -15,7 +17,6 @@ fetch('https://restcountries.eu/rest/v2/all?fields=iso2Code;name')
         })
         // Download selected country data
         select.addEventListener('change', () => {
-            // 
             const selectedItem = select.options[select.selectedIndex].value;
             fetch(`https://restcountries.eu/rest/v2/name/${selectedItem}`)
                 .then(resp =>
@@ -23,6 +24,7 @@ fetch('https://restcountries.eu/rest/v2/all?fields=iso2Code;name')
                 )
                 .then(resp => {
                     resp.forEach(item => {
+                        // Get country data
                         countryData.innerHTML = `
                     <h3 class="country-name">
                         ${item.name}
@@ -42,6 +44,20 @@ fetch('https://restcountries.eu/rest/v2/all?fields=iso2Code;name')
                     <div>
                         Strefa czasowa: <strong>${item.timezones}</strong>
                     </div>`;
+                        // Get country flag
+                        flagImg.src = item.flag;
+                        // Get country map
+                        const latApi = item.latlng[0];
+                        const lngApi = item.latlng[1];
+                        console.log(latApi)
+                        console.log(lngApi)
+                        countryMap = L.map('mapid').setView([latApi, latApi], 5);
+                        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                            maxZoom: 18,
+                            id: 'mapbox/streets-v11',
+                            accessToken: 'pk.eyJ1IjoiZGFtamFjayIsImEiOiJjazQzcXphMmQwN2s1M2ZvNjZ1YnBpN2NrIn0.QmvaGn94IKlvYRQn7ZSXFQ'
+                        }).addTo(countryMap);
                     })
                 })
         })
