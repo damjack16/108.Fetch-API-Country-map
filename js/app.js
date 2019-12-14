f76b58eaab8be148cc0d731e4d623dc319cdac29
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2019-12-14 10:45:05 +0100

    Complete downloading maps from Leaflet.js library

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const select = document.querySelector("#countrySelect");
 const countryData = document.querySelector("#countryData");
 const flagImg = document.querySelector("#countryFlag");
 let countryMap = document.querySelector("#mapid");
+countryMap = L.map('mapid');
 
 fetch('https://restcountries.eu/rest/v2/all?fields=iso2Code;name')
     .then(resp =>
@@ -30,34 +31,35 @@ fetch('https://restcountries.eu/rest/v2/all?fields=iso2Code;name')
                         ${item.name}
                     </h3>
                     <div>
-                        Stolica: <strong>${item.capital}</strong>
+                        Capital: <strong>${item.capital}</strong>
                     </div>
                     <div>
                         Region: <strong>${item.region}</strong>
                     </div>
                     <div>
-                        Podregion: <strong>${item.subregion}</strong>
+                        Subregion: <strong>${item.subregion}</strong>
                     </div>
                     <div>
-                        Liczba ludności: <strong>${item.population}</strong>
+                        Population: <strong>${item.population}</strong>
                     </div>
                     <div>
-                        Strefa czasowa: <strong>${item.timezones}</strong>
+                        Timezone: <strong>${item.timezones}</strong>
                     </div>`;
                         // Get country flag
                         flagImg.src = item.flag;
                         // Get country map
                         const latApi = item.latlng[0];
                         const lngApi = item.latlng[1];
-                        console.log(latApi)
-                        console.log(lngApi)
-                        countryMap = L.map('mapid').setView([latApi, latApi], 5);
+                        // Set coordinates to map from Leaflet.js
+                        countryMap.setView([latApi, lngApi], 5);
                         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
                             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                             maxZoom: 18,
                             id: 'mapbox/streets-v11',
                             accessToken: 'pk.eyJ1IjoiZGFtamFjayIsImEiOiJjazQzcXphMmQwN2s1M2ZvNjZ1YnBpN2NrIn0.QmvaGn94IKlvYRQn7ZSXFQ'
                         }).addTo(countryMap);
+                        console.log(latApi);
+                        console.log(lngApi)
                     })
                 })
         })
